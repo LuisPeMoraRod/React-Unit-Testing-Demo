7d1c4da7262f8b2bf3c40db2a5058c401b8cd813
Luis Pedro Morales <luismorarod98@gmail.com>
2022-09-20 13:27:52 -0600

    feat(select): install react-select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import SW_logo from './SW-logo.svg'
 import './App.css';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={SW_logo} className="SW-logo" alt="logo" />
+
       </header>
+      <body>
+        <p>
+          Choose a Star Wars character to retrieve some meta-data from {" "}
+          <a
+            className="App-link"
+            href="https://swapi.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SWAPI
+          </a>
+        </p>
+        <Form.Select className='App-select'>
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+      </body>
+
     </div>
   );
 }
