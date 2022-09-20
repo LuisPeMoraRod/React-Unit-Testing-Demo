fb8fcf1f52642dca81204d0b333a81f8f61144e2
Luis Pedro Morales <luis.morales.rodriguez@intel.com>
2022-09-20 14:44:42 -0600

    feat(data fetch): add axios method to retrieve all people

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
+import React, {  useState, createRef, useEffect } from "react";
 import SW_logo from './SW-logo.svg'
 import './App.css';
-import Form from 'react-bootstrap/Form';
+import Select from "react-select";
+const axios = require("axios");
 
 function App() {
 
+  const [charactersData, setCharactersData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const parseData = (data) => {
+    return data.map((character) => {
+      return {...character, value: character.name, label: character.name}
+    });
+  }
+
+  useEffect(()=>{
+    // fetch("https://swapi.dev/api/people/")
+    //   .then((response) => {
+    //     if (response.ok) return response.json();
+    //     throw response;
+    //   })
+    //   .then((data) => {
+    //     const parsedData = parseData(data.results);
+    //     console.log(parsedData)
+    //     setCharactersData(parsedData);
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error fetching data: ", error);
+    //     setError(error);
+    //   })
+    //   .finally(() => setLoading(false));
+    let people = [];
+    axios("https://swapi.co/api/people/")
+        .then(response => {
+            people = response.data.results;
+            return response.data.count;
+        })
+        .then(count => {
+            // exclude the first request
+            const numberOfPagesLeft = Math.ceil((count - 1) / 10);
+            let promises = [];
+            // start at 2 as you already queried the first page
+            for (let i = 2; i <= numberOfPagesLeft; i++) {
+                promises.push(axios(`https://swapi.co/api/people?page=${i}`));
+            }
+            return Promise.all(promises);
+        })
+        .then(response => {
+            //get the rest records - pages 2 through n.
+            people = response.reduce((acc, data) => [...acc, ...data.data.results], people);
+            const parsedData = parseData(people.results);
+            console.log(parsedData)
+            setCharactersData(parsedData);
+        })
+        .catch((error) => {
+              console.error("Error fetching data: ", error);
+              setError(error);
+            })
+        .finally(() => setLoading(false));
+  }, [])
+
+  if (loading) return "Loading...";
+  if (error) return "Error!";
   return (
     <div className="App">
       <header>
@@ -22,12 +82,12 @@ function App() {
             SWAPI
           </a>
         </p>
-        <Form.Select className='App-select'>
-          <option>Open this select menu</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </Form.Select>
+        <Select
+           className="basic-single"
+           classNamePrefix="select"
+           name="characters"
+           options={charactersData}
+        />
       </body>
 
     </div>
